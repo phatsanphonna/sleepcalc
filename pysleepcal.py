889c16f2c3a007e99a8386e4bdf2b0d95577a12a
ssuniie <phatsanphon2004@outlook.com>
2021-01-15 11:20:01 +0700

    change file surname

--- pysleepcal.py
+++ pysleepcal.py
@@ -1,7 +1,7 @@
 # pysleepcal.py
 
 print("How many hours have you sleep?")
-sleep = (int(input(">>> ")))
+sleep = int(input(">>> "))
 
 if sleep <= 5:
     print("Do you have any problem in sleeping? (Y/N)")
@@ -10,7 +10,7 @@ if sleep <= 5:
         print("Go back to sleep now!")
     elif sleep_problem == 'N':
         print("You should sleep more!")
-elif sleep > 5 & sleep <= 8:
+elif sleep > 5 and sleep <= 8:
     print("Good Job!")
 elif sleep < 32:
     print("You sleep too much!")
