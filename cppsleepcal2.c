4a38251a8b998f2dde4b659f97528da70139e51b
ssuniie <phatsanphon2004@outlook.com>
2021-01-17 00:36:33 +0700

    update if condition

--- cppsleepcal2.c
+++ cppsleepcal2.c
@@ -4,14 +4,31 @@
 int main()
 {
     int sleep;
+    char sleep_problem;
 
     printf("How many Hour have you sleep?");
     printf("\n>>> ");
     scanf("%d", &sleep);
     
     if (sleep <= 5)
+        printf("\nDo you have problem in sleeping?");
+        printf("\n>>> ");
+        scanf("%s", &sleep_problem);
+    
     {
+        switch (sleep_problem)
+        {
+        case 'Y':
+            printf("\nGo back to sleep now!");
+            break;
         
+        case 'N':
+            printf("\nYou should sleep more!");
+            break;
+        
+        default:
+            break;
+        }
     }
     
 }
